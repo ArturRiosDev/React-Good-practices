3a63ad6e71036cc68bb1168f9301d80f464522f3
Arturo Rios <arturo_rios@epam.com>
2022-07-17 14:26:03 -0500

    lists

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+// import { SplitScreen } from "./SplitScreen";
+
+//LayOut component
+// const LeftHandComponent = ({name})=>{
+//   return <p style={{background:'green'}}>{name}</p>
+// }
+
+// const RightHandComponents = ({name})=>{
+//   return <p style={{background:'red'}}>{name}</p>
+// }
+
+//List and list items 
+
+const people = [
+  {
+    id:'123',
+    name:'jhon doe',
+    hairColor:'brown',
+    hobbies:['swimming','bicycling','video games']
+  },
+  {
+    id:'12',
+    name:'juan',
+    hairColor:'red',
+    hobbies:['swimming','bicycling','video games']
+  },
+  {
+    id:'23',
+    name:'Gabriel',
+    hairColor:'blue',
+    hobbies:['swimming','video games']
+  }
+]
+
+
 
 function App() {
   return (
-    <h1>Your code goes here</h1>
+    // <SplitScreen left={LeftHandComponent} right={RightHandComponents} leftWeight={1} rightWeight={3}/>
+    // <SplitScreen leftWeight={1} rightWeight={1}>
+    //   <LeftHandComponent name={'hello'}/>
+    //   <RightHandComponents name={'there'}/>
+    // </SplitScreen>
+    <>
+    </>
   );
 }
 
